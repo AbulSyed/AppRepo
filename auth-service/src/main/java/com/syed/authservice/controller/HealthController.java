a39743a9bbddbc314fba91a3ce92df169decd65f
Abul Syed <abulsyed@Abuls-MacBook-Air.local>
2023-01-27 20:43:20 +0000

    spring profiles active

--- auth-service/src/main/java/com/syed/authservice/controller/HealthController.java
+++ auth-service/src/main/java/com/syed/authservice/controller/HealthController.java
@@ -1,5 +1,7 @@
 package com.syed.authservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HealthController.class);
+
     @Value("${pom.version}")
     private String pomVersion;
 
     @GetMapping("/health")
     public String health() {
+        LOGGER.debug("Entering HealthController:health");
+        
         return "Version " + pomVersion + " of Auth API service up";
     }
 }

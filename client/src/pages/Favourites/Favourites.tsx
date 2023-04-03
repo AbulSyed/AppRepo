8cb27e2347b23dae02cfe1f82ddcdb67c5cd62c7
Abul Syed <syed1234@hotmail.co.uk>
2023-04-03 21:56:13 +0100

    category repo map

--- client/src/pages/Favourites/Favourites.tsx
+++ client/src/pages/Favourites/Favourites.tsx
@@ -1,14 +1,33 @@
 import { Breadcrumb, Divider, Empty } from 'antd'
 import CategoryTable from '../../components/CategoryTable';
 import { useAppSelector } from "../../store/hooks";
+import Header from './Header';
 
 const Favourites: React.FC = () => {
-  // const starredRepos = useAppSelector((state) => state.repo.starredRepos);
   const sharedRepos = useAppSelector((state) => state.repo.sharedRepos);
 
   // filtering sharedRepos for items that have been 'starred'
   let starredRepos = [];
   starredRepos = sharedRepos.filter(obj => obj.starred);
+
+  // map containing category as 'key' and list of repos as 'value'
+  function getCategoryRepoMap(starredRepos: any) {
+    const map: any = {};
+  
+    for (const repoDto of starredRepos) {
+      const tempKey = repoDto.category;
+      
+      if (!map[tempKey]) {
+        map[tempKey] = [];
+      }
+      
+      map[tempKey].push(repoDto);
+    }
+    
+    return map;
+  }
+
+  const starredReposMap = getCategoryRepoMap(starredRepos);
 
   return (
     <div>
@@ -24,17 +43,15 @@ const Favourites: React.FC = () => {
 
       {
       // issue with the 'star' feature not in sync
-      // starredRepos ?
-      //   Object.keys(starredRepos).map(key => {
-      //     return (
-      //       <div key={key}>
-      //         <Header title={key} />
-      //         <CategoryTable sharedRepos={starredRepos[key]} />
-      //       </div>
-      //     )
-      //   })
-      starredRepos ?
-        <CategoryTable sharedRepos={starredRepos} />
+      Object.keys(starredReposMap).length != 0 ?
+        Object.keys(starredReposMap).map(key => {
+          return (
+            <div key={key}>
+              <Header title={key} />
+              <CategoryTable sharedRepos={starredReposMap[key]} />
+            </div>
+          )
+        })
         :
         <Empty
           image={Empty.PRESENTED_IMAGE_SIMPLE}

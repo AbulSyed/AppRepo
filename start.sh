eb777676e1e50ba7396d527100fea98e619ee0b4
Abul Syed <syed1234@hotmail.co.uk>
2023-04-06 00:41:39 +0100

    docker config

--- start.sh
+++ start.sh
@@ -23,6 +23,12 @@ mvn spring-boot:run &
 REPO_SERVICE_PID=$!
 cd ..
 
+echo "starting feedback-service"
+cd ./feedback-service
+mvn spring-boot:run &
+FEEDBACK_SERVICE_PID=$!
+cd ..
+
 echo "starting client"
 cd ./client
 npm run dev
@@ -30,7 +36,7 @@ CLIENT_PID=$!
 cd ..
 
 # Trap command used to capture the interrupt signal (ctrl + c) and execute the specified command to shut down all processes
-trap "echo 'Shutting down'; pkill -P $EUREKA_SERVER_PID; pkill -P $GATEWAY_SERVER_PID; pkill -P $AUTH_SERVICE_PID; pkill -P $REPO_SERVICE_PID; pkill -P $CLIENT_PID; exit 0" INT
+trap "echo 'Shutting down'; pkill -P $EUREKA_SERVER_PID; pkill -P $GATEWAY_SERVER_PID; pkill -P $AUTH_SERVICE_PID; pkill -P $REPO_SERVICE_PID; pkill -P $FEEDBACK_SERVICE_PID; pkill -P $CLIENT_PID; exit 0" INT
 
 # Wait for all processes to finish
 wait

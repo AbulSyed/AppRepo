67b6c2f95a43445c31f7ca581f7ece07d488304a
Abul Syed <syed1234@hotmail.co.uk>
2023-04-05 23:48:39 +0100

    get feedback endpoint

--- feedback-service/src/main/java/com/syed/feedbackservice/service/FeedbackService.java
+++ feedback-service/src/main/java/com/syed/feedbackservice/service/FeedbackService.java
@@ -2,7 +2,11 @@ package com.syed.feedbackservice.service;
 
 import com.syed.feedbackservice.dto.FeedbackDto;
 
+import java.util.List;
+import java.util.Map;
+
 public interface FeedbackService {
 
     String postFeedback(FeedbackDto feedbackDto);
+    Map<String, List<FeedbackDto>> getFeedback();
 }
--- feedback-service/src/main/java/com/syed/feedbackservice/service/impl/FeedbackServiceImpl.java
+++ feedback-service/src/main/java/com/syed/feedbackservice/service/impl/FeedbackServiceImpl.java
@@ -5,10 +5,19 @@ import com.syed.feedbackservice.dto.FeedbackDto;
 import com.syed.feedbackservice.entity.Feedback;
 import com.syed.feedbackservice.repository.FeedbackRepository;
 import com.syed.feedbackservice.service.FeedbackService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class FeedbackServiceImpl implements FeedbackService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FeedbackServiceImpl.class);
 
     private final FeedbackRepository feedbackRepository;
     private final ObjectMapper mapper;
@@ -20,6 +29,8 @@ public class FeedbackServiceImpl implements FeedbackService {
 
     @Override
     public String postFeedback(FeedbackDto feedbackDto) {
+        LOGGER.info("Entering FeedbackServiceImpl:postFeedback");
+
         Feedback feedback = mapper.convertValue(feedbackDto, Feedback.class);
 
         feedbackRepository.save(feedback);
@@ -27,5 +38,35 @@ public class FeedbackServiceImpl implements FeedbackService {
         // sending string as we don't need to send object back to client
         // users will not see their submitted feedback, only admins will...
         return "Feedback shared";
+    }
+
+    @Override
+    public Map<String, List<FeedbackDto>> getFeedback() {
+        LOGGER.info("Entering FeedbackServiceImpl:getFeedback");
+
+        List<Feedback> feedbacks = feedbackRepository.findAll();
+        List<FeedbackDto> feedbackDtos = new ArrayList<>();
+
+        for (Feedback feedback : feedbacks) {
+            FeedbackDto feedbackDto = mapper.convertValue(feedback, FeedbackDto.class);
+            feedbackDtos.add(feedbackDto);
+        }
+
+        return getFeedbackMap(feedbackDtos);
+    }
+
+    private Map<String, List<FeedbackDto>> getFeedbackMap(List<FeedbackDto> feedbackDtos) {
+        Map<String, List<FeedbackDto>> map = new HashMap<>();
+
+        for (FeedbackDto feedbackDto : feedbackDtos) {
+            String tempKey = feedbackDto.getArea().toString();
+
+            if (!map.containsKey(tempKey)) {
+                map.put(tempKey, new ArrayList<>());
+            }
+            map.get(tempKey).add(feedbackDto);
+        }
+
+        return map;
     }
 }

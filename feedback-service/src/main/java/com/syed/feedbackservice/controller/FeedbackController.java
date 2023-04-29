87f2489fde5707b52fea6011ff24d1f15059c6bb
Abul Syed <syed1234@hotmail.co.uk>
2023-04-29 22:13:03 +0100

    FeedbackAPI swagger config

--- feedback-service/src/main/java/com/syed/feedbackservice/controller/FeedbackController.java
+++ feedback-service/src/main/java/com/syed/feedbackservice/controller/FeedbackController.java
@@ -2,6 +2,10 @@ package com.syed.feedbackservice.controller;
 
 import com.syed.feedbackservice.dto.FeedbackDto;
 import com.syed.feedbackservice.service.FeedbackService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
+@Api(value = "Feedback controller exposes endpoints: shareFeedback, getFeedback, feedback")
 @RestController
 public class FeedbackController {
 
@@ -26,6 +31,13 @@ public class FeedbackController {
      * @param feedbackDto the feedback containing area and comment
      * @return string method that feedback has been shared
      */
+    @ApiOperation(value = "Endpoint to share feedback")
+    @ApiResponses({
+            @ApiResponse(
+                    code = 201,
+                    message = "CREATED"
+            )
+    })
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/shareFeedback")
     public String postFeedback(@RequestBody FeedbackDto feedbackDto) {
@@ -38,6 +50,15 @@ public class FeedbackController {
      * endpoint to get map containing feedback
      * @return feedback as map
      */
+    @ApiOperation(value = "Endpoint to get map containing feedback")
+    @ApiResponses({
+            @ApiResponse(
+                    code = 200,
+                    message = "OK",
+                    response = Map.class,
+                    responseContainer = "List"
+            )
+    })
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/getFeedback")
     public Map<String, List<FeedbackDto>> getFeedback() {
@@ -52,6 +73,18 @@ public class FeedbackController {
      * @param feedbackDto the update object
      * @return the updated feedback object
      */
+    @ApiOperation(value = "Endpoint which allows admins to update 'resolved' status")
+    @ApiResponses({
+            @ApiResponse(
+                    code = 200,
+                    message = "OK",
+                    response = FeedbackDto.class
+            ),
+            @ApiResponse(
+                    code = 404,
+                    message = "Feedback not found"
+            )
+    })
     @ResponseStatus(HttpStatus.OK)
     @PutMapping("/feedback/{feedbackId}")
     public FeedbackDto updateFeedback(@PathVariable Long feedbackId, @RequestBody FeedbackDto feedbackDto) {

2ca41fbb64169d21ca122af903dd4208d27724f6
Abul Syed <syed1234@hotmail.co.uk>
2023-05-27 20:49:53 +0100

    updated mvn build script

--- mavenbuild.sh
+++ mavenbuild.sh
@@ -1,25 +1,25 @@
 #chmod +x ./mavenbuild.sh - to make executable
 echo "building auth-service"
 cd ./auth-service
-mvn clean install
+mvn package -DskipTests
 cd ..
 
 echo "building eureka-server"
 cd ./eureka-server
-mvn clean install
+mvn package -DskipTests
 cd ..
 
 echo "building gateway-server"
 cd ./gateway-server
-mvn clean install
+mvn package -DskipTests
 cd ..
 
 echo "building repo-service"
 cd ./repo-service
-mvn clean install
+mvn package -DskipTests
 cd ..
 
 echo "building feedback-service"
 cd ./feedback-service
-mvn clean install
+mvn package -DskipTests
 cd ..

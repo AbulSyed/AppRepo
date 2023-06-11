87ed05bfd96353e369191db4e0a8edf53ee1cab5
Abul Syed <syed1234@hotmail.co.uk>
2023-06-11 13:05:37 +0100

    mysql

--- mysql/database_apprepo.sql
+++ mysql/database_apprepo.sql
@@ -1,23 +1,23 @@
 create database apprepo;
 use apprepo;
 
-create table user (
-  id bigint not null auto_increment,
-  avatar_url varchar(255),
-  github_url varchar(255),
-  repo_url varchar(255),
-  token varchar(255),
-  username varchar(255),
-  primary key (id)
-) engine=InnoDB;
+-- create table user (
+--   id bigint not null auto_increment,
+--   avatar_url varchar(255),
+--   github_url varchar(255),
+--   repo_url varchar(255),
+--   token varchar(255),
+--   username varchar(255),
+--   primary key (id)
+-- ) engine=InnoDB;
 
-create table repo (
-  id bigint not null auto_increment,
-  category varchar(255),
-  clone_url varchar(255),
-  description varchar(255),
-  html_url varchar(255),
-  language varchar(255),
-  name varchar(255),
-  primary key (id)
-) engine=InnoDB;
+-- create table repo (
+--   id bigint not null auto_increment,
+--   category varchar(255),
+--   clone_url varchar(255),
+--   description varchar(255),
+--   html_url varchar(255),
+--   language varchar(255),
+--   name varchar(255),
+--   primary key (id)
+-- ) engine=InnoDB;
